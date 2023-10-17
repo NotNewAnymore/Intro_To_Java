b67a70d26d9972a4bc4f11eb47a5845d771e30be
NotNewAnymore <Henryfleischer01@gmail.com>
2023-10-16 19:42:18 -0700

    Created project
    Not much else to say. It's a basic Java project.

---
+++ src/main/java/com/henryfleischer/intro_to_java/Demonstration.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.henryfleischer.intro_to_java;
+
+/**
+ *
+ * @author Not New Anymore
+ */
+public class Demonstration {
+    public static void main(String[] args) {
+        System.out.println("TesTesTest");
+    }
+}
